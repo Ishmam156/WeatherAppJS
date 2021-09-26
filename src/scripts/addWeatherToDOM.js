34f2e05ffd7b539248b420821f19fa7da17ddea3
Ishmam Chowdhury <ishmamchowdhurygp@gmail.com>
2021-09-26 18:11:56 +0600

    DOM changes on API call done

--- src/scripts/addWeatherToDOM.js
+++ src/scripts/addWeatherToDOM.js
@@ -1,5 +1,24 @@
+import { capitalize } from "./helper";
+
 const displayWeatherToDOM = (data) => {
   console.log(data);
+
+  document.getElementById("tempValue").textContent = Math.round(data.main.temp);
+  document.getElementById(
+    "tempPicture"
+  ).src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  document.getElementById("tempDescription").textContent = capitalize(
+    data.weather[0].description
+  );
+  const date = new Date();
+  document.getElementById("tempTime").textContent = date.toUTCString();
+  document.getElementById("windValue").textContent = `${data.wind.speed} m/s`;
+  document.getElementById(
+    "humidityValue"
+  ).textContent = `${data.main.humidity}%`;
+  document.getElementById(
+    "pressureValue"
+  ).textContent = `${data.main.pressure} hPa`;
 };
 
 export { displayWeatherToDOM };
